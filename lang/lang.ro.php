7c92dd7e8833123fc8f7b990a047afdd48f20d05
Vlad <vlad_micutul@yahoo.com>
2011-02-09 09:37:34 +0200

    Added Romanian translation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,24 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad_micutul@yahoo.com',
+			'website' => ''
+		),
+		'release-date' => '2011-02-09'
+	);
+
+	/**
+	 * Field: HTML Panel
+	 */
+	$dictionary = array(
+
+		'HTML Panel' =>
+		'Panou HTML',
+		
+		'URL Expression' =>
+		'Expresie URL',
+	
+	);
