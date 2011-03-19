1f8c5e0d2df8ba6ef4ada7620b5d8807acb2ede1
michael-e <info@michael-eichelsdoerfer.de>
2011-03-20 02:18:53 +0800

    cURL shouldn't try to verify the peer certificate.

--- fields/field.html_panel.php
+++ fields/field.html_panel.php
@@ -84,6 +84,7 @@
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			curl_setopt( $ch, CURLOPT_COOKIE, $cookie);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
