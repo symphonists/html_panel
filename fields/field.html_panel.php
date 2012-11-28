7670546eb58d8d5f6d0337b3516b2300dc40b2ae
Dev_NIX <devnix.code@gmail.com>
2012-11-28 19:43:48 +0100

    Fixed safe_mode and open_basedir bug. PD: works for me on Symphony 2.3.1 :)

--- fields/field.html_panel.php
+++ fields/field.html_panel.php
@@ -81,12 +81,13 @@
 			session_write_close();
 
 			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_COOKIE, $cookie);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-			$result = curl_exec($ch);
+
+			$result = curl_exec_follow($ch);
+
 			curl_close($ch);
 						
 			// a unique name for this panel instance
@@ -179,3 +180,48 @@
 		}
 						
 	}
+
+function curl_exec_follow(/*resource*/ $ch, /*int*/ &$maxredirect = null) { 
+    $mr = $maxredirect === null ? 5 : intval($maxredirect); 
+    if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) { 
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0); 
+        curl_setopt($ch, CURLOPT_MAXREDIRS, $mr); 
+    } else { 
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); 
+        if ($mr > 0) { 
+            $newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+
+            $rch = curl_copy_handle($ch); 
+            curl_setopt($rch, CURLOPT_HEADER, true); 
+            curl_setopt($rch, CURLOPT_NOBODY, true); 
+            curl_setopt($rch, CURLOPT_FORBID_REUSE, false); 
+            curl_setopt($rch, CURLOPT_RETURNTRANSFER, true); 
+            do { 
+                curl_setopt($rch, CURLOPT_URL, $newurl); 
+                $header = curl_exec($rch); 
+                if (curl_errno($rch)) { 
+                    $code = 0; 
+                } else { 
+                    $code = curl_getinfo($rch, CURLINFO_HTTP_CODE); 
+                    if ($code == 301 || $code == 302) { 
+                        preg_match('/Location:(.*?)\n/', $header, $matches); 
+                        $newurl = trim(array_pop($matches)); 
+                    } else { 
+                        $code = 0; 
+                    } 
+                } 
+            } while ($code && --$mr); 
+            curl_close($rch); 
+            if (!$mr) { 
+                if ($maxredirect === null) { 
+                    trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING); 
+                } else { 
+                    $maxredirect = 0; 
+                } 
+                return false; 
+            } 
+            curl_setopt($ch, CURLOPT_URL, $newurl); 
+        } 
+    } 
+    return curl_exec($ch); 
+} 
